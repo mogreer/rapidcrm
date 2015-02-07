956b4298f3c550f579b64879c2819f6c8b2d3c1c
Morris Greer <mogreer1@gmail.com>
2015-02-07 23:28:30 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  resources :leads
+
+  get 'welcome/home'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+  
+  root :to => 'welcome#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
